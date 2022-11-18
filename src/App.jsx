50a639867761f4edadb8ccbac987b28e1625330e
Sibindra <timalsinasibndra@gmail.com>
2022-11-18 11:03:51 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import LoginContainer from "./components/LoginContainer";
+
+function App() {
+  return (
+    <div id="hero">
+        <LoginContainer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/LoginContainer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+
+// bootstarp css import
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+export default function LoginContainer() {
+    return (
+        <>
+            <Card>
+                <Card.Title>Example</Card.Title>
+                <Card.Body>Body Text</Card.Body>
+                <Card.Text>Card Text example</Card.Text>
+            </Card>
+        </>
+    );
+}
